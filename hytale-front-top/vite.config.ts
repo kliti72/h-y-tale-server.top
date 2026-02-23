94c3d8964e82b78441835a65b17da3a868f03a87
kliti elezi <elezikliti@gmail.com>
2026-02-23 23:38:31 +0100

    allowedHosts

--- hytale-front-top/vite.config.ts
+++ hytale-front-top/vite.config.ts
@@ -8,5 +8,13 @@ export default defineConfig({
     host: true,          // oppure '0.0.0.0' se preferisci esplicito
     port: 80,          // opzionale
     open: false,         // non apre automaticamente il browser sulla VPS
+      allowedHosts: [
+      'play.pvpshield.com',      // esatto dominio che usi
+      '.pvpshield.com',          // opzionale: permette tutti i subdomain (*.pvpshield.com)
+      'h-y-tale-server.com',
+      'www.h-y-tale-server.com',
+      '.h-y-tale-server.com'
+    ],
   },
+
 })
