9e962a8d1985efc5ef8cf1d579e96e2a4110e515
kliti elezi <elezikliti@gmail.com>
2026-02-23 23:31:34 +0100

    Create Prod Launch

--- hytale-front-top/vite.config.ts
+++ hytale-front-top/vite.config.ts
@@ -4,4 +4,9 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
+  server: {
+    host: true,          // oppure '0.0.0.0' se preferisci esplicito
+    port: 80,          // opzionale
+    open: false,         // non apre automaticamente il browser sulla VPS
+  },
 })
